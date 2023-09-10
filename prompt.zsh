d8371cca816ce0a811fdd022e9a39a29c9b7868f
Madjes <Mauricio.291202@gmail.com>
2023-09-10 11:42:23 -0600

    Added prompt

---
+++ prompt.zsh
@@ -0,0 +1,18 @@
+# Custom prompt
+source ./git-prompt.sh
+export GIT_PS1_SHOWDIRTYSTATE=1
+autoload -U colors && colors
+
+function precmd() {
+    echo -ne "\033]0;${PWD##*/}\007"
+    GIT_THING=`__git_ps1 " \uE085 %s"`
+
+    VENV_THING="%{$fg[cyan]%}"`[ -n $VIRTUAL_ENV ] && echo $VIRTUAL_ENV_PROMPT`"%{$reset_color%}"
+    # VTE_PWD_THING=`__vte_osc7`
+
+    PROMPT_ICON="%{$fg_bold[gray]%}["$'\uE11F'" %{$reset_color%} "
+    PROMPT=$PROMPT_ICON$VENV_THING"%{$fg_bold[cyan]%}%M@%n%{$reset_color%} %{$fg_bold[white]%}%%:%{$reset_color%} "
+
+    RPROMPT="%{$fg_bold[gray]%}%~%{$reset_color%} %{$fg_bold[green]%}"$GIT_THING"%{$reset_color%}"
+}
+
